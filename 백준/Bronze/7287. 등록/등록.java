c64f2876510dcc62a9eec4e31972b6a1c43d4e21
hyungun <chlgusrbs0808@gmail.com>
2026-04-03 13:21:57 +0900

    [Bronze V] Title: 등록, Time: 92 ms, Memory: 13840 KB -BaekjoonHub

--- 백준/Bronze/7287. 등록/등록.java
+++ 백준/Bronze/7287. 등록/등록.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("5");
+        System.out.println("10");
         System.out.println("chlgusrbs");
     }
 }
