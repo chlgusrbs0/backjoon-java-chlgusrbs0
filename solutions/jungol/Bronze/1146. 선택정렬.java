d76ce1a089a4b01f07cf388ce2357814fb8ed150
chlgusrbs <chlgusrbs0808@gmail.com>
2026-05-28 23:51:12 +0900

    jungol 1146번 문제 풀이 추가

---
+++ solutions/jungol/Bronze/1146. 선택정렬.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < N - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < N; j++) {
+                if (arr[min] > arr[j]) {
+                    min = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+
+            for (int k = 0; k < N; k++) {
+                System.out.print(arr[k] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
